1964c429edca964b464d6ad65f76b70e397bd620
WickyNilliams <WickyNilliams@MBA>
2022-03-02 15:18:12 +0000

    add example login component utilising nord components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Header, Stack } from "@nordhealth/react";
+import "./App.css";
+import { Login } from "./Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header>
+        <h1 className="n-typescale-l">Nord / React Example</h1>
+      </Header>
+      <main className="n-reset n-stack-horizontal">
+        <Stack className="stack">
+          <Login />
+        </Stack>
+      </main>
+    </>
   );
 }
 
