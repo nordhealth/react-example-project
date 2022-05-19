362a2b05dcb5a4690095030dc3a70845f97398ef
WickyNilliams <WickyNilliams@MBA>
2022-05-19 16:38:08 +0100

    upgrade to v18 react, use new rendering API

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import "@nordhealth/css";
 import App from "./App";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
