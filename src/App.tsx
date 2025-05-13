5a16a87ce458708e73608156e96e536a314c5455
Elwin van Eede <elwinvaneede@gmail.com>
2025-05-13 14:07:52 +0200

    Initialize project using `react-ts` template of `create-vite`

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React from "react";
-import { Header, Stack } from "@nordhealth/react";
-import "./App.css";
-import { Login } from "./Login";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Header>
-        <h1 className="n-typescale-l">Nord / React Example</h1>
-      </Header>
-      <main className="n-reset n-stack-horizontal">
-        <Stack className="stack">
-          <Login />
-        </Stack>
-      </main>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
